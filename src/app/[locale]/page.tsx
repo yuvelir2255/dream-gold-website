585b2e1f346cb6c40e338cb23c071192344e3c84
yuvelir2255 <vladbabii31@gmail.com>
2026-06-15 18:45:34 +0200

    Этап 1: сборка главной страницы

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,13 @@
 import {setRequestLocale} from 'next-intl/server';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+import Hero from '@/components/home/Hero';
+import CreateYourOwn from '@/components/home/CreateYourOwn';
+import FeaturedWorks from '@/components/home/FeaturedWorks';
+import WhatWeCan from '@/components/home/WhatWeCan';
+import Trust from '@/components/home/Trust';
+import Reviews from '@/components/home/Reviews';
+import FinalCta from '@/components/home/FinalCta';
 
 export default async function HomePage({
   params
@@ -9,10 +16,19 @@ export default async function HomePage({
 }) {
   const {locale} = await params;
   setRequestLocale(locale);
+
   return (
     <>
       <Header />
-      <main className="min-h-[40vh]" />
+      <main>
+        <Hero />
+        <CreateYourOwn />
+        <FeaturedWorks />
+        <WhatWeCan />
+        <Trust />
+        <Reviews />
+        <FinalCta />
+      </main>
       <Footer />
     </>
   );
